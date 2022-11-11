5de709dbdfb53f74d16a246f29339545d79df935
i7eo <admin@i7eo.com>
2022-11-11 16:29:51 +0800

    test: :white_check_mark: use tsup&esbuild test build

--- packages/constants/src/node/pkg.ts
+++ packages/constants/src/node/pkg.ts
@@ -1,5 +1,3 @@
 export const PKG_PREFIX = '@google-translate-select'
 export const PKG_NAME = 'google-translate-select'
 export const PKG_CAMELCASE_NAME = 'GoogleTranslateSelect'
-export const PKG_CAMELCASE_LOCAL_NAME = 'GoogleTranslateSelectLocale'
-export const PKG_BRAND_NAME = 'GoogleTranslateSelect'
--- packages/vue3/src/index.ts
+++ packages/vue3/src/index.ts
@@ -1,4 +1,4 @@
-import { PKG_BRAND_NAME } from '@google-translate-select/constants'
+import { PKG_CAMELCASE_NAME } from '@google-translate-select/constants'
 
-console.log(PKG_BRAND_NAME)
+console.log(PKG_CAMELCASE_NAME)
 console.log('@google-translate-select/vue3')
