d78cf3eb863e982a4e98279752d230da0ecdfdb3
i7eo <admin@i7eo.com>
2022-12-30 13:42:19 +0800

    docs: :memo: update config

--- apps/docs/.vitepress/config.ts
+++ apps/docs/.vitepress/config.ts
@@ -1,54 +1,59 @@
 import consola from 'consola'
-import { docsDirName, REPO_BRANCH, REPO_PATH } from './paths'
-import { languages } from './utils/lang'
-import { head } from './config/head'
-import { sidebars } from './config/sidebars'
-import { nav } from './config/nav'
-import { mdPlugin } from './config/plugins'
-import { features } from './config/features'
-import {PKG_CAMELCASE_SPACE_NAME as title} from '@google-translate-select/constants'
+import { docDirName } from './path'
+// import { languages } from './utils/lang'
+// import { head } from './config/head'
+// import { sidebars } from './config/sidebars'
+// import { nav } from './config/nav'
+// import { mdPlugin } from './config/plugins'
+// import { features } from './config/features'
+import {
+  PKG_CAMELCASE_SPACE_NAME as title,
+  REPO_BRANCH,
+  REPO_PATH,
+} from '@google-translate-select/constants'
 import type { UserConfig } from 'vitepress'
 
 export const config: UserConfig = {
   title,
-  description: '🚀 A modern, high-performance monorepo project designed with Google Translate JS SDK. Vue and react are supported!',
+  description:
+    '🚀 A modern, high-performance monorepo project designed with Google Translate JS SDK. Vue and react are supported!',
   lastUpdated: true,
-  head,
+  // head,
   themeConfig: {
     repo: REPO_PATH,
     docsBranch: REPO_BRANCH,
-    docsDir: docsDirName,
+    docsDir: docDirName,
 
     editLinks: true,
     editLinkText: 'Edit this page on GitHub',
     lastUpdated: 'Last Updated',
 
-    logo: '/images/element-plus-logo.svg',
-    logoSmall: '/images/element-plus-logo-small.svg',
-    sidebars,
-    nav,
-    agolia: {
-      apiKey: '377f2b647a96d9b1d62e4780f2344da2',
-      appId: 'BH4D9OD16A',
-    },
-    features,
-    langs: languages,
+    // logo: '/images/element-plus-logo.svg',
+    // logoSmall: '/images/element-plus-logo-small.svg',
+    // sidebars,
+    // nav,
+    // agolia: {
+    //   apiKey: '377f2b647a96d9b1d62e4780f2344da2',
+    //   appId: 'BH4D9OD16A',
+    // },
+    // features,
+    // langs: languages,
   },
 
-  locales,
+  // locales,
 
-  markdown: {
-    config: (md) => mdPlugin(md),
-  },
+  // markdown: {
+  //   config: (md) => mdPlugin(md),
+  // },
 
-  vue: {
-    template: {
-      ssr: true,
-      compilerOptions: {
-        directiveTransforms: buildTransformers(),
-      },
-    },
-  },
+  // vue: {
+  //   template: {
+  //     ssr: true,
+  //     compilerOptions: {
+  //       directiveTransforms: buildTransformers(),
+  //     },
+  //   },
+  // },
 }
 
 export default config
