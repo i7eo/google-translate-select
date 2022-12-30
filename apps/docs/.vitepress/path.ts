ac9e2f8169779340e88ed2c2d486988f8efda42e
i7eo <admin@i7eo.com>
2022-12-30 11:36:34 +0800

    chore: :hammer: stop dependbot, use renovate

---
+++ apps/docs/.vitepress/path.ts
@@ -0,0 +1,5 @@
+import path from 'path'
+
+export const docRoot = path.resolve(__dirname, '..')
+export const vpRoot = path.resolve(docRoot, '.vitepress')
+export const docDirName = 'docs'
--- packages/constants/src/node/pkg.ts
+++ packages/constants/src/node/pkg.ts
@@ -1,3 +1,4 @@
 export const PKG_PREFIX = '@google-translate-select'
 export const PKG_NAME = 'google-translate-select'
 export const PKG_CAMELCASE_NAME = 'GoogleTranslateSelect'
+export const PKG_CAMELCASE_SPACE_NAME = 'Google Translate Select'
