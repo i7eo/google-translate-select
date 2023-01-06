2fe25ee632b0401798f3c1271835e2023178238f
YeSuX <351490949@qq.com>
2023-01-06 10:54:37 +0800

    docs: :memo: add docs

--- apps/docs/.vitepress/config.ts
+++ apps/docs/.vitepress/config.ts
@@ -1,59 +1,30 @@
-import consola from 'consola'
-import { docDirName } from './path'
-// import { languages } from './utils/lang'
-// import { head } from './config/head'
-// import { sidebars } from './config/sidebars'
-// import { nav } from './config/nav'
-// import { mdPlugin } from './config/plugins'
-// import { features } from './config/features'
 import {
-  PKG_CAMELCASE_SPACE_NAME as title,
-  REPO_BRANCH,
+  PKG_PREFIX,
   REPO_PATH,
+  REPO_BRANCH,
 } from '@google-translate-select/constants'
-import type { UserConfig } from 'vitepress'
 
-export const config: UserConfig = {
-  title,
+export default {
+  lang: 'en-US',
+  title: PKG_PREFIX,
   description:
-    '🚀 A modern, high-performance monorepo project designed with Google Translate JS SDK. Vue and react are supported!',
+    'A modern, high-performance monorepo project designed with Google Translate JS SDK. Vue and react are supported!s',
   lastUpdated: true,
-  // head,
-  themeConfig: {
-    repo: REPO_PATH,
-    docsBranch: REPO_BRANCH,
-    docsDir: docDirName,
-
-    editLinks: true,
-    editLinkText: 'Edit this page on GitHub',
-    lastUpdated: 'Last Updated',
-
-    // logo: '/images/element-plus-logo.svg',
-    // logoSmall: '/images/element-plus-logo-small.svg',
-    // sidebars,
-    // nav,
-    // agolia: {
-    //   apiKey: '377f2b647a96d9b1d62e4780f2344da2',
-    //   appId: 'BH4D9OD16A',
-    // },
-    // features,
-    // langs: languages,
+  markdown: {
+    // theme: 'material-palenight',
+    lineNumbers: true,
   },
-
-  // locales,
-
-  // markdown: {
-  //   config: (md) => mdPlugin(md),
-  // },
-
-  // vue: {
-  //   template: {
-  //     ssr: true,
-  //     compilerOptions: {
-  //       directiveTransforms: buildTransformers(),
-  //     },
-  //   },
-  // },
+  themeConfig: {
+    siteTitle: PKG_PREFIX,
+    // nav: [{ text: 'Guide', link: '/guide' }],
+    socialLinks: [{ icon: 'github', link: `https://github.com/${REPO_PATH}` }],
+    editLink: {
+      pattern: `https://github.com/${REPO_PATH}/edit/${REPO_BRANCH}/apps/docs/:path`,
+      text: 'Edit this page on GitHub',
+    },
+    footer: {
+      message: 'Released under the MIT License.',
+      // copyright: 'Copyright (c) 2021 i7eo <https://github.com/i7eo>',
+    },
+  },
 }
-
-export default config
